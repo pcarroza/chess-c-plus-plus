9e98fa45fd813c1e62cc5c4d57e6afd201f9419c
Pablo Carroza <pcarroza.drimo@bicehipotecaria.cl>
2026-02-12 19:14:00 -0300

    save

--- src/models/modules/game/pieces/rules/KingRuleBasedCoordinateGenerator.cpp
+++ src/models/modules/game/pieces/rules/KingRuleBasedCoordinateGenerator.cpp
@@ -10,17 +10,18 @@ namespace models::modules::game::pieces::rules
     void KingRuleBasedCoordinateGenerator::generate()
     {
         const int simpleStep = 1;
+        const int column = 0;
 
         possibleMoves.clear();
 
         possibleMoves = {
             std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(simpleStep, simpleStep))),
-            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(simpleStep, 0))),
+            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(simpleStep, column))),
             std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(simpleStep, -simpleStep))),
-            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(0, simpleStep))),
-            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(0, -simpleStep))),
+            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(column, simpleStep))),
+            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(column, -simpleStep))),
             std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(-simpleStep, simpleStep))),
-            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(-simpleStep, 0))),
+            std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(-simpleStep, column))),
             std::shared_ptr<Coordinate>(piece->getDisplacedBy(Coordinate(-simpleStep, -simpleStep)))};
 
         possibleMoves.remove_if([](const std::shared_ptr<Coordinate> &coordinate)
