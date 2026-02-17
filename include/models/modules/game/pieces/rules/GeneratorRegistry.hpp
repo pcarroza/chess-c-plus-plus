7a411e0d9791705907d6304dcddcd2d8f1d4b7ec
Pablo Carroza <pcarroza.drimo@bicehipotecaria.cl>
2026-02-17 10:19:44 -0300

    save

--- include/models/modules/game/pieces/rules/GeneratorRegistry.hpp
+++ include/models/modules/game/pieces/rules/GeneratorRegistry.hpp
@@ -8,21 +8,14 @@ namespace models::modules::game::pieces::rules
 
 namespace models::modules::game::pieces::rules
 {
-
     class GeneratorRegistry
     {
     public:
         static void registerAll(MovementRulesGeneratorFactory &factory);
 
     private:
-
         GeneratorRegistry() = delete;
-    
-        static void registerBasicPieces(MovementRulesGeneratorFactory &factory);
-    
-        static void registerSpecialPieces(MovementRulesGeneratorFactory &factory);
     };
-
 }
 
 #endif
